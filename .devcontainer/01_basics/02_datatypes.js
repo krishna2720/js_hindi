2761f9de59cc981b6edd38523c65932d53c7a3bc
krishna2720 <krishna.125766@gmail.com>
2025-02-21 18:51:06 +0000

    datatype_understanding

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+// alert(3+3)
+//we are using node js not browser
+console.log(3+3);
+let name="krishna"
+let age=20
+let is_log_in=false
+console.log(typeof is_log_in)
+console.log(typeof null)    ///type of null is object
+console.log(typeof undefined)  //type of undefined is undefined
