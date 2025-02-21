f3abad1e605efaaf1880d51196c64bd61cdc5844
krishna2720 <krishna.125766@gmail.com>
2025-02-21 18:29:24 +0000

    container

---
+++ .devcontainer/01_basics/variables_basics.js
@@ -0,0 +1,12 @@
+const account_id=9923103031
+let account_email="krishna.125766@gmail.com"
+var password="3214"
+city="dhampur"
+//account_id=876   //not allowed eror comes if you do ..
+account_email="shaurya@gmail.com"
+password="321"
+city="bijnaur"
+console.log(account_id);
+console.log(account_email);
+console.log(password);
+console.log(city);
