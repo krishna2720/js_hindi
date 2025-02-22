c0e24fb274c626c7acd31d7beab01114559045ed
krishna2720 <krishna.125766@gmail.com>
2025-02-22 17:06:54 +0000

    datatype conersion basic

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,29 @@
+let score="33"
+console.log(typeof(score));
+let score_int=Number(score);
+console.log(typeof(score_int));
+//invalid string
+let sscore="33abc"
+console.log(typeof(sscore));
+let sscore_int=Number(sscore);
+console.log(typeof(sscore_int));
+console.log(sscore_int);
+//null value
+let ssscore=null
+console.log(typeof(ssscore));
+let ssscore_int=Number(ssscore);
+console.log(typeof(ssscore_int));
+console.log(ssscore_int);
+//undefined value
+let vcore=undefined
+console.log(typeof(vcore));
+let vcore_int=Number(vcore);
+console.log(typeof(vcore_int));
+console.log(vcore_int);
+//boolean value
+let vvcore=true
+console.log(typeof(vvcore));
+let vvcore_int=Number(vvcore);
+console.log(typeof(vvcore_int));
+console.log(vvcore_int);
+
--- .devcontainer/01_basics/variables_basics.js
+++ .devcontainer/01_basics/variables_basics.js
@@ -1,11 +1,11 @@
 const account_id=9923103031
 let account_email="krishna.125766@gmail.com"
-var password="3214"
+var password="3214"  //prefer not to use var because of block scope..
 city="dhampur"
 //account_id=876   //not allowed eror comes if you do ..
 account_email="shaurya@gmail.com"
 password="321"
-city="bijnaur"
+city="bijnaurs"
 console.log(account_id);
 console.log(account_email);
 console.log(password);
