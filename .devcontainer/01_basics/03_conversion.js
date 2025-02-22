9e6542fdab9e17a40e3e3b937de882ea354c9267
krishna2720 <krishna.125766@gmail.com>
2025-02-22 17:33:15 +0000

    conversion basic

--- .devcontainer/01_basics/03_conversion.js
+++ .devcontainer/01_basics/03_conversion.js
@@ -26,4 +26,29 @@ console.log(typeof(vvcore));
 let vvcore_int=Number(vvcore);
 console.log(typeof(vvcore_int));
 console.log(vvcore_int);
-
+console.log(" ");
+///****operations****//
+let v=40;
+let nv=-v;
+console.log(nv);
+console.log(2+2);
+console.log(2-2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+let a="krishna";
+let b=" agarwal";
+console.log(a+b);
+//problems
+console.log("2"+3)
+console.log("22"+"3")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log("1"+2+"2")
+console.log(3+4*5%3)
+console.log(true)
+console.log(+true)  //1
+console.log(+ " ")  //0
+let ab=100;
+ab++;
+console.log(ab);
