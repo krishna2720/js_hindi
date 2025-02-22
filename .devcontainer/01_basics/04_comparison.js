d7c1b6dd3bf36ac7feda8d4f9ea9fcc42976b3c1
krishna2720 <krishna.125766@gmail.com>
2025-02-22 17:58:14 +0000

    comparison basics

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,29 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2<=1)
+console.log(2==1)
+console.log(2!=1)
+console.log("2">1)
+console.log("02">1)
+console.log("");
+console.log(null>0)
+console.log(null<0)
+console.log(null==0)
+console.log(null!=0)
+console.log(null>=0)
+console.log(null<=0)
+console.log("");
+console.log(undefined>0)
+console.log(undefined<0)
+console.log(undefined==0)
+console.log(undefined!=0)
+console.log(undefined>=0)
+console.log(undefined<=0)
+// === operator use case
+console.log("the === operator result")
+console.log("2"==2)
+console.log("2"===2)
+
+
+
