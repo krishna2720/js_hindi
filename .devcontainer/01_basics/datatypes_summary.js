8f4e8b1d5b433b601ca64fa641120c43b89e75b0
krishna2720 <krishna.125766@gmail.com>
2025-02-22 18:47:07 +0000

    datatype_summary

---
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -0,0 +1,30 @@
+const score=100
+const scorevalue=100.3
+const ismale=true
+const money=null
+const id=Symbol("123")
+const did=Symbol("123")
+console.log(id);
+console.log(did);
+console.log(id==did);  //false
+console.log(id===did);   //false
+const bignumber=732728362846;
+console.log(bignumber)
+// arrays
+let arr=["krishna","papa","mummy"];    
+console.log(arr);
+console.log(typeof arr);
+//objects
+let myobj={
+    name:"krishnaa",
+    age:20,
+    address:"kharikuan",
+};
+
+console.log(typeof myobj);
+//functions
+const temp=function(){
+    console.log("hello world");
+};
+
+console.log(typeof temp);
